7674769076c1842764bc7693db84ede83741243d
David Montavon <david.montavon@gmail.com>
2020-05-14 11:54:17 +0200

    Fixed winning, some refactoring and code organization, added more controls in the top score retrieval/saving.

--- ajax.php
+++ ajax.php
@@ -3,13 +3,10 @@ header("Content-Type: application/json");
 
 function sanitize($str)
 {
-    $str = str_replace("<", "", $str);
-    $str = str_replace(">", "", $str);
-    $str = str_replace("\"", "", $str);
+    $str = trim($str);
+    $str = filter_var($str, FILTER_SANITIZE_STRING);
     if (strlen($str) > 10)
-    {
         $str = substr($str, 0, 10);
-    }
     return $str;
 }
 
@@ -20,31 +17,57 @@ if (isset($_GET["action"]))
     switch($action)
     {
         case "getTopScores":
-            $strTopScores = file_get_contents("top_scores.txt");
-            $lines = explode("\n", $strTopScores);
-            $jsonScores = array();
-            foreach ($lines as $line)
+            try
             {
-                if (strpos($line, ';') !== false)
+                $strTopScores = file_get_contents("top_scores.txt");
+                $lines = explode("\n", $strTopScores);
+                $jsonScores = array();
+
+                foreach ($lines as $line)
                 {
+                    if (strpos($line, ';') == false)
+                        continue;  
+
                     $fields = explode(";", $line);
-                    array_push($jsonScores, "{ \"name\": \"" . $fields[0] . "\", \"score\": " . $fields[1] . " }");
+                    $name = sanitize($fields[0]);
+                    $score = sanitize($fields[1]);
+
+                    if (strlen($name) == 0 || strlen($score) == 0 || !is_numeric($score))
+                        continue;
+
+                    array_push($jsonScores, "{ \"name\": \"" . $name . "\", \"score\": " . $score . " }");
                 }
+                echo "[" . implode(",", $jsonScores) . "]";
             }
-            echo "[" . implode(",", $jsonScores) . "]";
+            catch(Exception $e)
+            {
+                echo "[]";
+            }
             break;
 
         case "saveTopScores":
-            if (isset($_POST["topScores"]) && strlen($_POST["topScores"]) < 1000)
+            try
             {
-                $jsonTopScores = json_decode($_POST["topScores"]);
-                $fw = fopen("top_scores.txt", "w+");
-                foreach ($jsonTopScores as $jsonTopScore)
+                if (isset($_POST["topScores"]) && strlen($_POST["topScores"]) < 1000)
                 {
-                    fwrite($fw, sanitize($jsonTopScore->name) . ";" . $jsonTopScore->score . "\n");
+                    $jsonTopScores = json_decode($_POST["topScores"]);
+                    $fw = fopen("top_scores.txt", "w+");
+
+                    foreach ($jsonTopScores as $jsonTopScore)
+                    {
+                        $name = sanitize($jsonTopScore->name);
+                        $score = sanitize($jsonTopScore->score);
+                        if (strlen($name) > 0 && strlen($score) > 0 && is_numeric($score))
+                            fwrite($fw, $name . ";" . $score . "\n");
+                    }
+
+                    fclose($fw);
+                    echo "{\"success\": true}";
                 }
-                fclose($fw);
-                echo "{\"success\": true}";
+            }
+            catch (Exception $e)
+            {
+                echo "{\"success\": false}";
             }
             break;
     }
